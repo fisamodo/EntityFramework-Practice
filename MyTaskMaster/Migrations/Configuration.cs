133fdd0cc5bea579565045891361900f922ed7ff
Filip Velebit Samodol <fisamodo@vsite.hr>
2020-03-26 22:31:27 +0100

    Connected my database in app.config and made configuration with Enable-Migrations

---
+++ MyTaskMaster/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+﻿namespace MyTaskMaster.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<MyTaskMaster.Model.tmDBContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(MyTaskMaster.Model.tmDBContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
