e26ba0131fdab3e70197b2eb336799d077483e2f
Filip Velebit Samodol <fisamodo@vsite.hr>
2020-03-26 23:50:08 +0100

    Added INSERT commands, updated database and checked a DB in managment studio for a confirmation

--- MyTaskMaster/Migrations/202003262244258_AddStatusSeedDataToDB.cs
+++ MyTaskMaster/Migrations/202003262244258_AddStatusSeedDataToDB.cs
@@ -7,10 +7,15 @@
     {
         public override void Up()
         {
+            Sql("INSERT INTO Status (Name) VALUES ('To Do');");
+            Sql("INSERT INTO Status (Name) VALUES ('In Progress');");
+            Sql("INSERT INTO Status (Name) VALUES ('Done');");
+
         }
-        
+
         public override void Down()
         {
+            //delete statements here
         }
     }
 }
