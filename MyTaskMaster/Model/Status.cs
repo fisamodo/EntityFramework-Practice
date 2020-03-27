c188a322111a8df859edc93aa884dfbbb304cb4e
Filip Velebit Samodol <fisamodo@vsite.hr>
2020-03-27 12:28:58 +0100

    override tostring to change status naming in combobox

--- MyTaskMaster/Model/Status.cs
+++ MyTaskMaster/Model/Status.cs
@@ -10,5 +10,10 @@ namespace MyTaskMaster.Model
     {
         public int Id { get; set; }
         public  string Name { get; set; }
+
+        public override string ToString()
+        {
+            return Name;
+        }
     }
 }
